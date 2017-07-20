e1b8c3ac78d099cda519fa36b7849eee49a72287
sktifa <sahbi.ktifa@4dconcept.fr>
2017-07-20 14:10:21 +0200

    enable scheduling

--- src/main/java/fr/efaya/config/WebSecurityConfig.java
+++ src/main/java/fr/efaya/config/WebSecurityConfig.java
@@ -1,12 +1,9 @@
 package fr.efaya.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,13 +11,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import javax.servlet.MultipartConfigElement;
-
 /**
  * Created by KTIFA FAMILY on 11/05/2016.
  */
 @Configuration
 @EnableWebSecurity
+@EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
